9075dea87063041caa86af7d3ead29fadc9b6247
Doug Shannon <doug@tensure.us>
2020-01-18 23:00:28 -0500

    #4

--- content/challenges/medium/4 - Reverse Words/func.js
+++ content/challenges/medium/4 - Reverse Words/func.js
@@ -4,4 +4,21 @@
 // Notes:
 //   - You can expect a valid string to be provided for each test case.
 module.exports = (str) => {
+  const words = str.split(' ');
+  const output = [];
+
+  for (let x = 0; x < words.length; x++) {
+    const curWord = words[x];
+    if (curWord.length > 4) {
+      let reversed = '';
+      for (let y = curWord.length - 1; y >= 0; y--) {
+        reversed += curWord[y];
+      }
+      output.push(reversed);
+    } else {
+      output.push(curWord);
+    }
+  }
+
+  return output.join(' ');
 }
