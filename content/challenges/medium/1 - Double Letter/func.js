2ec8e273716a601fa900a799720e4f50ceb4d058
Doug Shannon <doug@tensure.us>
2020-01-18 19:18:23 -0500

    solution to medium 1

--- content/challenges/medium/1 - Double Letter/func.js
+++ content/challenges/medium/1 - Double Letter/func.js
@@ -1,3 +1,14 @@
 // Challenge link here: https://edabit.com/challenge/YHLaB8itA9tEDN5TG
 module.exports = (word) => {
+    let lastletter;
+    for (let i = 0; i < word.length; i++){
+        const letter = word[i];
+        if (lastletter) {
+            if (letter === lastletter) {
+                return true;
+            }
+        }
+        lastletter = letter;
+    }
+    return false;
 }
