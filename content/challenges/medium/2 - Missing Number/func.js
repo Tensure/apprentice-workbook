051bea213213957089f6f09afc35319ceba780f5
Doug Shannon <doug@tensure.us>
2020-01-18 21:11:53 -0500

    2 and 3 done

--- content/challenges/medium/2 - Missing Number/func.js
+++ content/challenges/medium/2 - Missing Number/func.js
@@ -1,3 +1,16 @@
 // Challenge link here: https://edabit.com/challenge/rgdT9sZ4QhsKhaSw9
 module.exports = (arr) => {
+    for (let num = 1; num < 11; num++) {
+        let found = false;
+        for (let i = 0; i < arr.length; i++) {
+            let curNum = arr[i];
+            if (curNum == num) {
+                found = true;
+            }
+        }
+        if (found == false) {
+            return num;
+        }
+    }
+    return -1;
 }
--- content/challenges/medium/3 - Remove Characters/func.js
+++ content/challenges/medium/3 - Remove Characters/func.js
@@ -1,3 +1,20 @@
 // Challenge link here: https://edabit.com/challenge/k7CwrsKPHrEL8h4z6
 module.exports = (str) => {
+    const badChars = ['!', '@', '#', '$', '%', '^', '&', '\\', '*', '(', ')', '.']
+    let output = '';
+    for (let curLetterIndex = 0; curLetterIndex < str.length; curLetterIndex++) {
+        let curLetter = str[curLetterIndex];
+        let isBadChar = false;
+        for(let curBadCharIndex = 0; curBadCharIndex < badChars.length; curBadCharIndex++) {
+            let curBadChar = badChars[curBadCharIndex];
+
+            if (curLetter == curBadChar){
+                isBadChar = true;
+            }
+        }
+        if (!isBadChar) {
+            output += curLetter;
+        }
+    }
+    return output;
 }
