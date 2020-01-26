f5cb04febe5d591f18f1d92ac3595bf9e07df648
Doug Shannon <doug@Tensure-DS.local>
2020-01-26 14:28:43 -0500

    GCD answer

--- content/challenges/medium/10 - Greatest Common Denominator/func.js
+++ content/challenges/medium/10 - Greatest Common Denominator/func.js
@@ -3,4 +3,21 @@
 //   - Both values will be positive.
 //   - The GCD is the largest factor that divides both numbers.
 module.exports = (int1, int2) => {
+    const big = int1 > int2 ? int1 : int2;
+    const little = int1 > int2 ? int2 : int1;
+
+    if (big % little == 0){
+        return little;
+    }
+
+    for (var num = Math.floor(little/2); num > 2; num--) {
+        if (little % num == 0) {
+            if (big % num == 0) {
+                return num;
+            }
+        }
+    }
+
+    return 1;
+
 }
--- content/challenges/medium/10 - Greatest Common Denominator/func.test.js
+++ content/challenges/medium/10 - Greatest Common Denominator/func.test.js
@@ -1,13 +1,21 @@
 const func = require('./func.js');
 
-test('Expect 8', () => {
+test('Expect 32, 8 > GCD 8', () => {
     expect(func(32, 8)).toBe(8);
 });
 
-test('Expect 4', () => {
+test('Expect 8, 12 > GCD 4', () => {
     expect(func(8, 12)).toBe(4);
 });
 
-test('Expect 1', () => {
+test('Expect 17, 13 > GCD 1', () => {
     expect(func(17, 13)).toBe(1);
+});
+
+test('Expect 2, 1 > GCD 1', () => {
+    expect(func(2, 1)).toBe(1);
+});
+
+test('Expect 2, 3 > GCD 1', () => {
+    expect(func(2, 1)).toBe(1);
 });
