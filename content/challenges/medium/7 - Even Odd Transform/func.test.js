c22586ad07a6d6dd9cd5d766f7a3480c4d74628b
Doug Shannon <wdouglasshannon@gmail.com>
2020-05-13 08:51:40 -0400

    Update to Equal

--- content/challenges/medium/7 - Even Odd Transform/func.test.js
+++ content/challenges/medium/7 - Even Odd Transform/func.test.js
@@ -1,13 +1,13 @@
 const func = require('./func.js');
 
 test('Expect result to be [9, -2, 15]', () => {
-    expect(func([3, 4, 9], 3)).toBe([9, -2, 15]);
+    expect(func([3, 4, 9], 3)).toEqual([9, -2, 15]);
 });
 
 test('Expect result to be [-20, -20, -20]', () => {
-    expect(func([0, 0, 0], 10)).toBe([-20, -20, -20]);
+    expect(func([0, 0, 0], 10)).toEqual([-20, -20, -20]);
 });
 
 test('Expect result to be [3, 0, 5]', () => {
-    expect(func([1, 2, 3], 1)).toBe([3, 0, 5]);
+    expect(func([1, 2, 3], 1)).toEqual([3, 0, 5]);
 });
