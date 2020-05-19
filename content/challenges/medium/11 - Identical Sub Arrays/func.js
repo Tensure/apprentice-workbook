511f58f55c1d50324a0549702a0fd1f0dc742048
Doug Shannon <doug@tensure.io>
2020-05-19 09:24:30 -0400

    clarification

--- content/challenges/medium/11 - Identical Sub Arrays/func.js
+++ content/challenges/medium/11 - Identical Sub Arrays/func.js
@@ -1,4 +1,5 @@
 // Create a function that takes in a two-dimensional array and returns the number of sub-arrays with identical elements.
+// e.g. [['#', '#'],[2,2,2]], both sub-arrays contain identical elements so this would return 2.
 // Notes: 
 //   - Single-element arrays count as (trivially) having identical elements.
 module.exports = (arr) => {
