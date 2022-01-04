9f84be9a988c156a2a9a51881fb7ed54b4bb7b9d
Doug Shannon <wdouglasshannon@gmail.com>
2022-01-03 19:49:46 -0500

    Update func.test.js

--- content/challenges/medium/3 - Remove Characters/func.test.js
+++ content/challenges/medium/3 - Remove Characters/func.test.js
@@ -4,7 +4,7 @@ test('Remove the "!"', () => {
     expect(func("The quick brown fox!")).toEqual("The quick brown fox");
 });
 test("Remove the '%', '(',')', '.''", () => {
-    expect(func("%fd76$fd(-)6GvKlO.")).toEqual("fd76fd-6GvKlO");
+    expect(func("%fd76$fd(-)6GvKlO.")).toEqual("fd76fd-6GvKlO.");
 });
 test('Remove the "$"', () => {
     expect(func("D0n$c sed 0di0 du1")).toEqual("D0nc sed 0di0 du1");
