a75d7454b2343c3ecae3e49fb3c3495bca29c6a2
Jason Brammer <brammer.jason@gmail.com>
2025-03-13 18:27:04 -0400

    Fixed another test

--- content/challenges/easy/8 - Blackjack/func.test.js
+++ content/challenges/easy/8 - Blackjack/func.test.js
@@ -3,8 +3,8 @@ const func = require("./func.js");
 test("Expected: 'False'", () => {
   expect(func(['A', 2, 3])).toEqual(false);
 });
-test("Expected: 'False'", () => {
-  expect(func(['A', 'J', 'K'])).toEqual(false);
+test("Expected: 'Blackjack!'", () => {
+  expect(func(['A', 'J', 'K'])).toEqual('Blackjack!');
 });
 test("Expected: 'true'", () => {
   expect(func(['A', 'J', 'K', 'Q'])).toEqual(true);
